57fdc260b692524ed26a5a160b4f2924a7ecd859
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-07 10:19:24 +0200

    Object Oriented Programming

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class Sqaure:
+    def __init__(self, size=0):
+        self.__size = size
+            
+    # Property
+    @property
+    def size(self):
+        return self.__size
+
+    # Setter modifies
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        size = self.__size
+        return size * size 
+
+    def my_print(self):
+        size = self.__size
+
+        if size == 0:
+            print()
+
+        for row in range(size):
+            print('#' * size)
