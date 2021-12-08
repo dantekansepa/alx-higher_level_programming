c1b7ef0ed0ac50f42032cef5142ae7f18a954b6e
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-08 22:48:57 +0200

    Updated

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+"""module contains class Square"""
+
+
 class Sqaure:
+   """ Retrieves the size of a square"""
     def __init__(self, size=0):
+        """initializes Square
+        Args:
+            __size: the size of square
+        """
         self.__size = size
-            
-    # Property
+        
     @property
     def size(self):
+        """Retrieves the size of square"""
         return self.__size
 
-    # Setter modifies
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError('size must be an integer')
+        """sets the size of square
+        Args:
+        value: size of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
-
-    def area(self):
-        size = self.__size
-        return size * size 
+            raise ValueError("size must be >= 0")
+        self.__size = value
