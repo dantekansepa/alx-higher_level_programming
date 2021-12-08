72ccbf7b62a265ff3f556a34ee1fd300e3505b93
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-08 22:15:29 +0200

    Documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,7 +6,8 @@ class Sqaure:
     """Instantiates a private instance attribute
     with no type/value verification"""
     def __init__(self, size):
-        """Private instance attribute:
-        size
+        """Initializes a square.
+        Arg:
+            size: size of the square
         """
         self.__size = size
