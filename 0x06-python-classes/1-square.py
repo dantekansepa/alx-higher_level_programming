9d350aa38f5938228b23d794ca841d4b1c1c60da
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-08 11:40:05 +0200

    Square object

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""module contains class Square"""
+
+
 class Sqaure:
+    """Instantiates a private instance attribute
+    with no type/value verification"""
     def __init__(self, size):
+        """Private instance attribute:
+        size
+        """
         self.__size = size
