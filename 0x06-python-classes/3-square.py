047c358bbcc262a09f85d34689ccdd6505ce8926
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-08 22:37:07 +0200

    OOP

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,7 +13,7 @@ class Sqaure:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-            self.__size = size
+        self.__size = size
             
     def area(self):
          """Returns the area of the current square"""
