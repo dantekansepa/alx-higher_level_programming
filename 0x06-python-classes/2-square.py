dd80196f938ee4dfbb38a0b63d659e539a55398f
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-08 14:37:16 +0200

    OOP

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""module contains class Square"""
+
 class Sqaure:
+    """validates the size of a square
+    """
     def __init__(self, size=0):
+        """Instantiation with optional size: def __init__(self, size=0):
+           size must be an integer, otherwise raise a TypeError exception 
+           with the message size must be an integer
+           if size is less than 0, raise a ValueError 
+           exception with the message size must be >= 0
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
