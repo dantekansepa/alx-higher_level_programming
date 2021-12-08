01068d1b89ca6e6b2663bf9ed5d24586ba3a978f
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-08 22:32:38 +0200

    Object

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 class Sqaure:
+     """has public instance area"""
     def __init__(self, size=0):
+        """validates the value and type of size
+        Args:
+        size: size of the square
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
             self.__size = size
             
     def area(self):
+         """Returns the area of the current square"""
         size = self.__size
         return size * size
