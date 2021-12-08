e6111303d43be036a05ac150726d1baf4146bf4e
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-08 22:46:18 +0200

    OOP

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,19 +2,19 @@
 """Module with an empty class"""
 
 
-class Sqaure:
-     """has public instance area"""
+class Square:
+    """has public instance area"""
     def __init__(self, size=0):
         """validates the value and type of size
         Args:
         size: size of the square
         """
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-            
+
     def area(self):
-         """Returns the area of the current square"""
+        """Returns the area of the current square"""
         return self.__size * self.__size
