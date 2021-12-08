d5374e97f05cfdf31d704148e68dc015bf9ea481
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-08 22:50:12 +0200

    Update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,15 +2,15 @@
 """module contains class Square"""
 
 
-class Sqaure:
-   """ Retrieves the size of a square"""
+class Square:
+    """ Retrieves the size of a square"""
     def __init__(self, size=0):
         """initializes Square
         Args:
             __size: the size of square
         """
         self.__size = size
-        
+
     @property
     def size(self):
         """Retrieves the size of square"""
@@ -26,3 +26,9 @@ class Sqaure:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+        self.__size = value
+
+    def area(self):
+        """returns the area of square"""
+        return self.__size * self.__size
