c48b513c2ff29ea597cf8d5aea95183d817dcd55
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-08 22:30:13 +0200

    OOP

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,16 +2,15 @@
 """module contains class Square"""
 
 
-class Sqaure:
+class Square:
     """validates the size of a square"""
     def __init__(self, size=0):
         """validates the type of size
         Args:
         size: int type square size
         """
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
