512a8b3f82e009cb7523b69e01f67576658b1c3c
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-12-08 22:52:06 +0200

    Object

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python3
-class Sqaure:
+"""module contains class Square"""
+
+
+class Square:
+    """defines a square"""
     def __init__(self, size=0):
+        """Initializes a square
+        Args:
+            __size: size of the squre
+        """
         self.__size = size
-            
-    # Property
+
     @property
     def size(self):
+        """Retrieves the size of square"""
         return self.__size
 
-    # Setter modifies
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError('size must be an integer')
+        """Sets the size of square
+        Args:
+            value: size to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-        size = self.__size
-        return size * size 
+        """Returns the area of a square"""
+        return self.__size * self__size
 
     def my_print(self):
-        size = self.__size
-
-        if size == 0:
-            print()
-
-        for row in range(size):
-            print('#' * size)
-            
+        """prints the square to stdout with #"""
+        loop = 0
+        if self.__size == 0:
+            print("")
+        else:
+            for loop in range(self.__size):
+                print('#' * self.__size)
